db2134e34d14823468b7a8d1896fa33c1ff9afc3
Alexey Gorbenko <a.v.gor@mail.ru>
2023-04-26 16:36:00 +0300

    refactor: rewrite btn light function

--- assets/js/lightBtns.js
+++ assets/js/lightBtns.js
@@ -1,11 +1,7 @@
-function highLightBtn(event) {
+export default function highLightBtn(event) {
   const btn = document.querySelector(`[data-code="${event.code}"]`);
+  if (event.repeat) {
+    btn.classList.remove('keyboard__btn_pressed');
+  }
   btn.classList.toggle('keyboard__btn_pressed');
 }
-
-function unHighLightBtn(event) {
-  const btn = document.querySelector(`[data-code="${event.code}"]`);
-  btn.classList.remove('keyboard__btn_pressed');
-}
-
-export { highLightBtn, unHighLightBtn };
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,24 @@
 import createBody from './createBody.js';
 import createButtons from './createBtns.js';
 import updBtnKeys from './updBtnKeys.js';
-import { highLightBtn, unHighLightBtn } from './lightBtns.js';
+import highLightBtn from './lightBtns.js';
 
 createBody();
 createButtons();
 updBtnKeys('en');
 
+function preventDefault(event) {
+  const preventArr = ['Tab', 'AltLeft'];
+
+  if (preventArr.includes(event.code)) {
+    event.preventDefault();
+  }
+
+  if (event.code === 'Tab') {
+    document.querySelector('.keyboard__input-field').value += '  ';
+  }
+}
+
+document.addEventListener('keydown', preventDefault);
 document.addEventListener('keydown', highLightBtn);
-document.addEventListener('keyup', unHighLightBtn);
+document.addEventListener('keyup', highLightBtn);
