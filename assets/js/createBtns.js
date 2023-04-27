b899a51f2a73bdeec0feea9d78d1534dd24cca9b
Alexey Gorbenko <a.v.gor@mail.ru>
2023-04-27 15:18:26 +0300

    refactor: rewrite code to minimize num of functions

--- assets/js/createBtns.js
+++ assets/js/createBtns.js
@@ -1,12 +1,1 @@
-export default function createButtons() {
-  const idsRow1 = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'];
-  const idsRow2 = ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete'];
-  const idsRow3 = ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'];
-  const idsRow4 = ['ShiftLeft', 'IntlBackslash', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight'];
-  const idsRow5 = ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
-  const ids = idsRow1.concat(idsRow2, idsRow3, idsRow4, idsRow5);
-  const btns = document.querySelectorAll('.keyboard__btn');
-  for (let i = 0; i < btns.length; i += 1) {
-    btns[i].dataset.code = ids[i];
-  }
-}
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,18 @@
 import createBody from './createBody.js';
-import createButtons from './createBtns.js';
 import updBtnKeys from './updBtnKeys.js';
 import lightBtn from './lightBtns.js';
 import preventDef from './preventDefault.js';
 import data from './data.js';
-import setLocalStorage from './setLocalStorage.js';
 import getLocalStorage from './getLocalStorage.js';
 import watchPressedBtns from './watchPressedBtns.js';
 
 function onLoadPage() {
   createBody();
-  createButtons();
   getLocalStorage();
   updBtnKeys(data.lang);
 }
 
 document.addEventListener('DOMContentLoaded', onLoadPage);
-document.addEventListener('unload', setLocalStorage);
 document.addEventListener('keydown', lightBtn);
 document.addEventListener('keyup', lightBtn);
 document.addEventListener('keydown', preventDef);
