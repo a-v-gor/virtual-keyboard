45a3f831e368796a28a32902ff4013ec52e59409
Alexey Gorbenko <a.v.gor@mail.ru>
2023-04-28 13:49:40 +0300

    fix: fix adding '&', '<', '>' symbols  to textarea

--- assets/js/preventDefault.js
+++ assets/js/preventDefault.js
@@ -8,13 +8,23 @@ export default function preventDef(event) {
   const usBtnArr = ['Backspace', 'Delete', 'Enter', 'ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'Space'];
 
   function addText(str) {
+    let copyStr;
+    if (str === '&amp;') {
+      copyStr = '&';
+    } else if (str === '&gt;') {
+      copyStr = '>';
+    } else if (str === '&lt;') {
+      copyStr = '<';
+    } else {
+      copyStr = str;
+    }
     const textArea = document.querySelector('.keyboard__input-field');
     const start = textArea.selectionStart;
     const end = textArea.selectionEnd;
-    const readyStr = textArea.value.substring(0, start) + str + textArea.value.substring(end);
+    const readyStr = textArea.value.substring(0, start) + copyStr + textArea.value.substring(end);
     textArea.value = readyStr;
     textArea.focus();
-    textArea.selectionEnd = (start === end) ? (end + str.length) : end;
+    textArea.selectionEnd = (start === end) ? (end + copyStr.length) : end;
   }
 
   if (!usBtnArr.includes(event.code)) {
