5d8cf61c9e2731ea2c2a9297a08490cd5e4b4a79
Alexey Gorbenko <a.v.gor@mail.ru>
2023-05-01 09:40:48 +0300

    rafactor: add Enter btn function through click

--- assets/js/preventDefault.js
+++ assets/js/preventDefault.js
@@ -3,10 +3,11 @@ import addText from './addText.js';
 import lightBtns from './lightBtns.js';
 
 export default function preventDef(event) {
+  console.log(event);
   const preventArr = fnBtns.concat(['Tab', 'CapsLock', 'ControlLeft',
     'MetaLeft', 'AltLeft', 'AltLeft', 'AltLeft', 'AltRight', 'ContextMenu', 'ControlRight']);
 
-  const usBtnArr = ['Backspace', 'ShiftLeft', 'ShiftRight', 'Delete', 'Enter', 'ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'Space'];
+  const usBtnArr = ['Backspace', 'ShiftLeft', 'ShiftRight', 'Delete', 'ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'Space'];
 
   if (!usBtnArr.includes(event.code)) {
     event.preventDefault();
@@ -14,6 +15,8 @@ export default function preventDef(event) {
   if (event.type === 'keydown') {
     if (event.code === 'Tab') {
       addText('  ');
+    } else if (event.code === 'Enter') {
+      addText('\n');
     } else if (!preventArr.includes(event.code) && !usBtnArr.includes(event.code)) {
       const btn = document.querySelector(`[data-code="${event.code}"]`);
       if (btn) {
