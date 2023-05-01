1ce4fb81e8959d8a0977be3cb4a7e23d4a89ffeb
Alexey Gorbenko <a.v.gor@mail.ru>
2023-05-01 23:59:23 +0300

    refactor: add focus on input after click

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ import updBtnKeys from './updBtnKeys.js';
 import preventDef from './preventDefault.js';
 import getLocalStorage from './getLocalStorage.js';
 import clickOnKbd from './clickOnKbd.js';
+import focusOnInput from './focusOnInput.js';
 
 function onLoadPage() {
   createBody();
@@ -13,5 +14,6 @@ function onLoadPage() {
 document.addEventListener('DOMContentLoaded', onLoadPage);
 document.addEventListener('keydown', preventDef);
 document.addEventListener('keyup', preventDef);
+document.addEventListener('mouseup', focusOnInput);
 document.addEventListener('mousedown', clickOnKbd);
 document.addEventListener('mouseup', clickOnKbd);
